62319b350bb20c1bdc9421067760b65973af43da
Swapnil Idawate <swapnilidawate103@gmail.com>
2024-09-04 00:09:19 +0530

    chages in get

--- BasicRequests/getmodeule.py
+++ BasicRequests/getmodeule.py
@@ -1,5 +1,10 @@
 import requests
 
-r = requests.get("https://www.codewithharry.com")
-a=r.text
+r = requests.get("http://localhost:3000/people?id=5")
+a=r.json()
+assert r.status_code==200
+print(r.status_code)
+print(r.text)
 print(a)
+print(a[0]["courses"][0])
+print(a[0]["id"])
